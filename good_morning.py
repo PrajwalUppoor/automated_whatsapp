81dc593e513059a913f635cd307e84be8d8ce1ff
PrajwalUppoor <36199449+PrajwalUppoor@users.noreply.github.com>
2020-04-27 02:15:45 +0530

    Update good_morning.py

--- good_morning.py
+++ good_morning.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'AC5d70e07ecc64ef53d409b3211111bbf4'
-auth_token = '83f2b14f2388bfe8e60879fb92cdf465'
+account_sid = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 client = Client(account_sid, auth_token)
 
 def sendmorningmessage():
